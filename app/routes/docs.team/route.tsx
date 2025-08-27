7e2f7c2e5a5af1790da347384e6f1ab42462cd84
Judy Racusin <judith.racusin@nasa.gov>
2025-08-27 16:00:35 -0400

    Link team page

--- app/routes/docs.team/route.tsx
+++ app/routes/docs.team/route.tsx
@@ -7,13 +7,24 @@
  */
 import { Link } from '@remix-run/react'
 import {
+  Button,
   CardBody,
+  CardFooter,
   CardGroup,
   CardHeader,
   CardMedia,
   GridContainer,
+  Icon,
+  Tag,
 } from '@trussworks/react-uswds'
 import type { ReactNode } from 'react'
+import { useState } from 'react'
+import { ReactTags } from 'react-tag-autocomplete'
+import type {
+  OptionRendererProps,
+  Tag as ReactTag,
+  TagRendererProps,
+} from 'react-tag-autocomplete'
 
 import type { BreadcrumbHandle } from '~/root/Title'
 
@@ -26,29 +37,93 @@ function TeamCard({
   name,
   affiliation,
   photo,
+  href,
+  tags,
+  selectedTags,
 }: {
   children: ReactNode
   name: string
   affiliation: string
   photo: string
+  href: string
+  tags: string[]
+  selectedTags: string[]
 }) {
+  const tagSet = new Set(tags)
   return (
     <>
-      <div className="usa-card__container margin-bottom-2 grid-col-4">
-        <CardMedia imageClass="bg-white margin-0">
-          <img src={photo} loading="lazy" alt="placeholder" />
-        </CardMedia>
-        <CardHeader>
-          <h4 style={{ marginBottom: '0px' }}>{name}</h4>
-          <p style={{ marginTop: '0px', fontSize: '12px' }}>{affiliation}</p>
-        </CardHeader>
-        <CardBody style={{ fontSize: '16px' }}>{children}</CardBody>
-      </div>
+      {(selectedTags.length == 0 ||
+        selectedTags.every((tag) => tagSet.has(tag))) && (
+        <CardGroup className="usa-card__container margin-bottom-2 grid-col-4">
+          <CardMedia imageClass="bg-white padding-0">
+            <img
+              src={photo}
+              width="40"
+              height="40"
+              loading="lazy"
+              alt="placeholder"
+            />
+          </CardMedia>
+          <CardHeader>
+            <h4 style={{ marginBottom: '0px' }}>
+              <a className="usa-link" href={href}>
+                <>{name}</>
+              </a>
+            </h4>
+            <p style={{ marginTop: '0px', fontSize: '12px' }}>{affiliation}</p>
+          </CardHeader>
+          <CardBody style={{ fontSize: '16px' }}>{children}</CardBody>
+          <CardFooter style={{ fontSize: '10px' }}>
+            {tags?.map((tag) => <Tag key={tag}>{tag}</Tag>)}
+          </CardFooter>
+        </CardGroup>
+      )}
     </>
   )
 }
 
+function renderTag({
+  classNames,
+  tag: { label },
+  ...tagProps
+}: TagRendererProps) {
+  return (
+    <Tag className={classNames.tag} {...tagProps}>
+      <span className={classNames.tagName}>
+        {label}{' '}
+        <Button type="button" unstyled>
+          <Icon.Close
+            role="presentation"
+            className="margin-left-1 text-bottom"
+            color="white"
+          />
+        </Button>
+      </span>
+    </Tag>
+  )
+}
+
+function renderOption({
+  children,
+  classNames,
+  option,
+  ...optionProps
+}: OptionRendererProps) {
+  const classes = [classNames.option]
+
+  if (option.active) classes.push(classNames.optionIsActive)
+
+  return (
+    <div className={classes.join(' ')} {...optionProps}>
+      {option.disabled ? children : <Tag>{children}</Tag>}
+    </div>
+  )
+}
+
 export default function () {
+  const [tags, setTags] = useState<ReactTag[]>([])
+  const suggestions = ['moderator', 'classic', 'kafka']
+  const tagNames = tags.map(({ label }) => label)
   return (
     <GridContainer className="usa-section">
       <h1>GCN Team</h1>
@@ -60,24 +135,63 @@ export default function () {
         </Link>{' '}
         .
       </p>
+      <div className="margin-bottom-2">
+        <ReactTags
+          onAdd={(tag) => setTags((prevTags) => [...prevTags, tag])}
+          onDelete={(i) =>
+            setTags((prevTags) => {
+              const newTags = prevTags.slice(0)
+              newTags.splice(i, 1)
+              return newTags
+            })
+          }
+          delimiterKeys={[' ', ',', 'Enter', 'Tab']}
+          collapseOnSelect
+          suggestions={suggestions.map((label) => ({ label, value: label }))}
+          selected={tags}
+          renderTag={renderTag}
+          renderOption={renderOption}
+          placeholderText="Filter by tag"
+        />
+      </div>
       <CardGroup>
-        <TeamCard name="Judy Racusin" affiliation="NASA/GSFC" photo={headshot}>
+        <TeamCard
+          name="Judy Racusin"
+          affiliation="NASA/GSFC"
+          photo={headshot}
+          href="https://github.com/jracusin"
+          tags={['moderator', 'classic']}
+          selectedTags={tagNames}
+        >
           Principal Investigator
         </TeamCard>
-        <TeamCard name="Leo Singer" affiliation="NASA/GSFC" photo={headshot}>
+        <TeamCard
+          name="Leo Singer"
+          affiliation="NASA/GSFC"
+          photo={headshot}
+          href="https://github.com/lpsinger"
+          tags={['moderator', 'kafka']}
+          selectedTags={tagNames}
+        >
           Lead Developer
         </TeamCard>
         <TeamCard
           name="Dakota Dutko"
           affiliation="NASA/GSFC/ADNET"
           photo={headshot}
+          href="https://github.com/dakota002"
+          tags={['moderator', 'kafka']}
+          selectedTags={tagNames}
         >
           Full-Stack Developer
         </TeamCard>
         <TeamCard
           name="Courey Elliott"
-          affiliation="Lousiana State University/ NextSource"
+          affiliation="LSU/NextSource"
           photo={headshot}
+          href="https://github.com/courey"
+          tags={['kafka']}
+          selectedTags={tagNames}
         >
           Full-Stack Developer
         </TeamCard>
@@ -85,13 +199,19 @@ export default function () {
           name="Vidushi Sharma"
           affiliation="NASA/GSFC/UMBC"
           photo={headshot}
+          href="https://github.com/vidushi-github"
+          tags={['moderator']}
+          selectedTags={tagNames}
         >
           Postdoc
         </TeamCard>
         <TeamCard
           name="Tyler Barna"
-          affiliation="University of Minnesota"
+          affiliation="UMN"
           photo={headshot}
+          href="https://github.com/tylerbarna"
+          tags={['']}
+          selectedTags={tagNames}
         >
           Ph.D. Student
         </TeamCard>
